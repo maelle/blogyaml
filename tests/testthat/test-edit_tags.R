ef9c77bd1865cbc4ac9ee0e640d33e511366fbb0
Maëlle Salmon <maelle@itsalocke.com>
2018-06-07 12:54:15 +0200

    do not test the app on Travis

--- tests/testthat/test-edit_tags.R
+++ tests/testthat/test-edit_tags.R
@@ -5,7 +5,7 @@ library("shinytest")
 test_that("edit_tags works", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
-
+  skip_on_travis()
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
   # which runs on Linux.
